0b54c438ceac1f4b5cb2e0049e8853376e7d1913
Noxy-03 <diegojimenezescutia077@gmail.com>
2025-08-22 09:01:46 -0600

    Update matrix_insertion_sort.c
    Checar ciclo for de m en linea 38

--- matrix_insertion_sort.c
+++ matrix_insertion_sort.c
@@ -24,54 +24,74 @@ void sort_matrix(int **matrix, int n) {
     // TODO: Implementa aquí el algoritmo.
     // Necesitarás el método de inserción,
     // pero recuerda aplicar la regla de mover toda la columna.
+    int key;
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            key = matrix[i][j];
+            int k = j-1;
+            while (k >= 0 && matrix[i][k] > key) {
+                for (int m = i; m < n; m++) {
+                    matrix[m][k+1] = matrix[m][k];
+                }
+                k--;
+            }
+            for (int m = i; m < n; m++) {
+                if(m == i){
+                    matrix[i][k+1] = key;
+                }else{
+                    matrix[m][k+1] = matrix[m][j];
+                }
+            }
+        }
+    }
 }
 
-int main() {
-    int n = 3;
-    // Reserva dinámica de la matriz
-    int **matrix = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
-        matrix[i] = (int *)malloc(n * sizeof(int));
-    }
-
-    // Ejemplo de entrada
-    int ejemplo[3][3] = {
-        {4, 7, 2},
-        {9, 5, 6},
-        {8, 1, 3}
-    };
-
-    // Copiar ejemplo a la matriz dinámica
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = ejemplo[i][j];
+    int main() {
+        int n = 3;
+        // Reserva dinámica de la matriz
+        int **matrix = (int **)malloc(n * sizeof(int *));
+        for (int i = 0; i < n; i++) {
+            matrix[i] = (int *)malloc(n * sizeof(int));
         }
-    }
-
-    printf("Matriz original:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", matrix[i][j]);
+        
+        // Ejemplo de entrada
+        int ejemplo[3][3] = {
+            {4, 7, 2},
+            {9, 5, 6},
+            {8, 1, 3}
+        };
+        
+        // Copiar ejemplo a la matriz dinámica
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = ejemplo[i][j];
+            }
         }
-        printf("\n");
-    }
-
-    // Llamada a la función que deben completar
-    sort_matrix(matrix, n);
-
-    printf("\nMatriz ordenada:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", matrix[i][j]);
+        
+        printf("Matriz original:\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d ", matrix[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
+        
+        // Llamada a la función que deben completar
+        sort_matrix(matrix, n);
+        
+        printf("\nMatriz ordenada:\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d ", matrix[i][j]);
+            }
+            printf("\n");
+        }
+        
+        // Liberar memoria
+        for (int i = 0; i < n; i++) {
+            free(matrix[i]);
+        }
+        free(matrix);
+        
+        return 0;
     }
-
-    // Liberar memoria
-    for (int i = 0; i < n; i++) {
-        free(matrix[i]);
-    }
-    free(matrix);
-
-    return 0;
-}
